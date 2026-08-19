1e8e177c8441f872db828b2c66ccf8ee26adb394
Gulshan-k-7 <gulshangk7878@gmail.com>
2026-08-19 12:41:05 +0530

    all setup configured

--- src/utils/ApiErrors.js
+++ src/utils/ApiErrors.js
@@ -1,14 +1,14 @@
 class ApiErrors extends Error {
-    constructor(message, statusCode, statck="", error =[]){
+    constructor(message, statusCode, stack="", error =[]){
         super(message);
         this.statusCode = statusCode;
-        this.statck = statck;
+        this.stack = stack;
         this.error = error;
         this.data = null;
         this.success = false;
         
-        if(statck){
-            this.stack = statck;
+        if(stack){
+            this.stack = stack;
         }else{
             Error.captureStackTrace(this, this.constructor);
         }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,15 +1,20 @@
-export {aysncHandler} 
 
-// const asyncHandler = (fn) => (req, res, next) => {
-//     Promise.resolve(fn(req, res, next)).catch(next);
+
+const asyncHandler = (requestHandler) => {
+    return (req ,res,next) =>{
+
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+    }
+}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);   
+//     } catch (err) {
+//         res.status(500).json({ success: false, message: "Internal Server Error" });                                                                                                     
+//         next(err);
+//     }   
 // }
 
-
-const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        await fn(req, res, next);   
-    } catch (err) {
-        res.status(500).json({ success: false, message: "Internal Server Error" });                                                                                                     
-        next(err);
-    }   
-}
+export {asyncHandler} 
